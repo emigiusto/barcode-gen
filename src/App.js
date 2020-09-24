75a59468b72b404c039e9e97bd1e30f3331565e6
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-09-24 20:58:09 +0200

    withPrinterTest1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState }  from 'react';
 import * as JsBarcode from 'jsbarcode'
+import $ from 'jquery'
+import * as html2canvas from 'html2canvas'
 
 import './App.css';
 
@@ -17,11 +19,28 @@ function App() {
 
   const updateCode = e => {
     if (e.target.value!=="") {
-      setCode(e.target.value)
+      setCode(e.target.value.toUpperCase())
     }else{
       setCode("........")
     }
   };
+
+  var printDiv = (divName) =>{
+      var element = $("#" + divName)[0];
+        html2canvas(element).then(function (canvas) {
+          var myImage = canvas.toDataURL("image/png");
+          var tmp = window.open("");  
+          
+          $(tmp.document.body)
+          .html("<img src=" + myImage + " alt=''>")
+          .ready(function () {
+              tmp.focus();
+              tmp.print();
+              tmp.close();
+          })
+          
+        });
+  }
 
   return (
     <div className="App">
@@ -29,16 +48,17 @@ function App() {
         <label>BARCODE GENERATOR</label>
         <input type="text" placeholder="Enter code to generate" onChange={updateCode}/>
       </form>
-      <div className="barcode-container">
+      <div id="printableArea" className="barcode-container">
         <div className="code-name-container">
             <h1>{code.substring(0, 2)}</h1>
             <h1>{code.substring(2, 4)}</h1>
             <h1>{code.substring(4, 6)}</h1>
         </div>
-        
-
         <svg  id="barcode"></svg>
       </div>
+
+      <input type="button" onClick={()=> printDiv('printableArea')} value="Print barcode"/>
+      <div id="GFG"></div>
       
     </div>
   );
